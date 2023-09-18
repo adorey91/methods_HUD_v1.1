bae8c771e04265d7fafd2fbfe71c81e529beedf8
Adrian Dorey <144232716+adorey91@users.noreply.github.com>
2023-09-18 09:53:58 -0300

    Addedvariables
    Will need to delete stuff but this is important to keep for later.

--- methods_HUD_v1.1/Program.cs
+++ methods_HUD_v1.1/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,8 +9,33 @@ namespace methods_HUD_v1._1
 {
     internal class Program
     {
+        public string game = "Method_HUD_v1.1";
+        public string playerName;
+        public float score = 0f;
+        public int health = 100;
+        public int lives = 3;
+        float multiplier = 0f;
+        string playerInput;
+
+        public void ShowHUD()
+        {
+            Console.WriteLine("Game Name: " + game);
+            Console.WriteLine("Player Name: " + playerName);
+            Console.WriteLine("Score: " + score + "| " + "Health: " + health + "| " + "Lives: " + lives + "| " + "Multiplier: " + multiplier);
+            Console.WriteLine("");
+
+        }
+
         static void Main(string[] args)
         {
+            ShowHUD();
+
+            Console.WriteLine("Enter Name: ");
+            playerName = Console.ReadLine();
+
+
         }
+
+        
     }
 }
